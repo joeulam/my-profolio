658eba441842cb6a6c3f1446ff5fdc9fc8d7b72a
Joey Lam <joeulam345@gmail.com>
2023-05-31 18:14:46 -0400

    Text css change

--- app/page.js
+++ app/page.js
@@ -14,20 +14,23 @@ export default function Home() {
           <img src="../User.png"/>
           <img src="../Document.png"/>
         </navbar>
-        <h1>
-          Im Joey Lam
+        <h1 class="name">
+          Im <span class="text-[#FF3364]">Joey</span> <span class="text-[#52BECC]">Lam</span>
         </h1>
         <h3>
           A passionate student from New York Interested in robotics,
           web development, Cybersecurity and machine learning.
         </h3>
-        <div class="socials">
-          <img src='../GitHub.png'></img>
-          <img src='../Instagram.png'></img>
-          <img src='../LinkedIn.png'></img>
-          <img src='../Email.png'></img>
+        <div>
+          <div class="socials">
+            <img src='../GitHub.png'></img>
+            <img src='../Instagram.png'></img>
+            <img src='../LinkedIn.png'></img>
+            <img src='../Email.png'></img>
 
+          </div>
         </div>
+        
         <h3>
           Im currently listening to:{"spotifyapi"}
         </h3>
