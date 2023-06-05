5861e7ac743444b276aafc56b8a33614577ae882
Joey Lam <joeulam345@gmail.com>
2023-06-04 21:28:06 -0400

    Firebase funct

--- app/spotify/api.js
+++ app/spotify/api.js
@@ -3,42 +3,36 @@ import React from 'react';
 export default async function Tst() {
   const music = await loadspotify();
   //var a = music.entries[1].API
-  return <p>Im currently listening to: {}</p>;
-}
-
-const client_id = "70adfcf4e5aa488e9d8d86274ab01561" // Your client id
-const client_secret = "265a505c48184865976f0ada6a90d9a1" // Your secret
-
-export async function loadspotify() {
-  var tokenjson = await getSpotifyToken()
-  var token = tokenjson.access_token
-  const url = `https://api.spotify.com/v1/me/player/currently-playing`;
-  var response = await fetch(url, {
-    headers: {
-        Authorization:`Bearer ${token}`,
-    },
-});
-console.log(response)
-
+  if(music.is_playing){
+    return <p>Im currently listening to: {music.item.name}</p>;
+  }
+  else{
+    return <p>Im currently listening to nothing</p>
+  }
 }
 
 
-
-async function getSpotifyToken() {
+  const clientId = '70adfcf4e5aa488e9d8d86274ab01561'; // Replace with your Spotify client ID
+  const redirectUri = 'http://localhost:3000/callback'; // Replace with your redirect URI
+  const clientSecret = '265a505c48184865976f0ada6a90d9a1'
+  const refresh_token = 'AQAfp3qUGQ1-hrJKBWQq18yBwdgLP--YFnu8WmkEObI1zoLnDKzk6ZKL6I9gmPf6QLS6GKEAqzeiVvGQDPhdF1LKIQsvjsGMCwld3xpEozBNQhJSnHnr7q_sRwHmqRaXhpQ'
+  const acctok = 'BQAtaPEaz_mApHEr3xDQin-3doOFd3ngFb9ExLhJ-09BmfOrqjmSPe5BC2LLSDlHnLC0cdnM6FDWm2RiB72BHrMB0MBfSg2aTidClpWCtoWj0jkPGr9NFxItIEGihEXoNw2RV3s3mWqxLnB5YGQB8O7Mh6YXtYweAiO8PJdcoZlNdPKacrfFst0NQ0U6CLxHky9NWsTlXOc'
+async function auth() {
   cache: "no-store"
-  const url = 'https://accounts.spotify.com/api/token';
+  const url = 'https://accounts.spotify.com./api/token';
   var response = await fetch(url, {
       method: 'POST',
       headers: {
-          'code':"AQC00ivmsrRMK99qmGaHDX9eRgpRUjWcAago7L7YO_s8_1Gxk8zX8C8koe5AoSkvE_R8CBjqitfe4OGpckBZ2x75-pwWL5w0faZNZLD-so9HhJdxlaalo6hKc2_ksNc4RImtZSwaeqFfppj-aX8pfJjhPMY4GNrJrvR1Y8zvHhG-wL5XZFmy5DDfiU5IjRYVNWqW4LDNx9QJMaMrsDs",
-          'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+          'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64')),
           'Content-Type': 'application/x-www-form-urlencoded'
       },
-      body: 'grant_type=authorization_code',
-      // client_credentials or authorization_code// 
-      scope:'user-read-currently-playing',
-      json: true
-  });
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token,
+      }),
+      cache: 'no-store',
+      revalidate: 10
+    });
   if (response.ok) {
       const jsonResponse = await response.json();
       console.log(jsonResponse);
@@ -48,3 +42,17 @@ async function getSpotifyToken() {
       throw new Error(`Request failed! Status code: ${response.status} ${response.statusText}`);
   }
 }
+
+export async function loadspotify() {
+  var token = await auth()
+  const url = `https://api.spotify.com./v1/me/player/currently-playing`;
+  var response = await fetch(url, {
+    headers: {
+        Authorization:`Bearer ${token.access_token}`,
+    },
+});
+if (response.ok) {
+  const jsonResponse = await response.json();
+  console.log(jsonResponse);
+  return jsonResponse
+}}
